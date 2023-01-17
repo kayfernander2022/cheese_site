9a52c856f489f29c9a75ad47f41142118b9bde2f
kayfernander2022 <kayfernander@hotmail.com>
2023-01-17 11:53:14 -0500

    CastError fix

--- src/actions.js
+++ src/actions.js
@@ -23,31 +23,27 @@ export const createAction = async ({ request }) => {
 }
 
 export const updateAction = async ({request, params}) => {
-  // get data from form
   const formData = await request.formData()
-  // set up our new person to match schema
+  
   const updatedCheese = {
     name: formData.get("name"),
     countryOfOrigin: formData.get("countryOfOrigin"),
     image: formData.get("image"),
   }
-  // Send new person to our API
+  
   await fetch(URL + "/cheese/" + params.id, {
       method: "put",
       headers: {
           "Content-Type":"application/json"
       },
       body: JSON.stringify(updatedCheese)
-  })
-  // redirect to index
+  }) 
   return redirect("/")
 }
 
 export const deleteAction = async ({params}) => {
-  // delete person with our API
   await fetch(URL + "/cheese/" + params.id, {
       method: "delete"
   })
-  // redirect to index
   return redirect("/")
 }
